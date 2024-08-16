5bbad56fcc4f5c2a6ce2e13600c9c88e61a7d3d0
Alex Collins <sullen.umbra@gmail.com>
2024-08-16 11:32:07 -0500

    Fix broken zip script

--- src/zip.ts
+++ src/zip.ts
@@ -4,6 +4,8 @@ const zip = new AdmZip()
 
 try {
     zip.addLocalFile('./manifest.json')
+    zip.addLocalFile('./popup.html')
+    zip.addLocalFile('./app_icon.png')
     zip.addLocalFolder('./build')
     zip.writeZip('output.zip')
 } catch (e) {
