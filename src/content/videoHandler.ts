3db6783fa919979bdc6303b2e39da11f055e5c4d
Alex Collins <sullenumbra@gmail.com>
2024-08-31 21:21:27 -0500

    Change timeout back

--- src/content/videoHandler.ts
+++ src/content/videoHandler.ts
@@ -45,7 +45,7 @@ export async function checkVideoProgress() {
         const video = await waitForVideo();
         const timeLeft = video.duration - video.currentTime;
         
-        if (timeLeft <= 0.1) {
+        if (timeLeft <= 0.3) {
             skipShort();
         }
         if (getIsRunning()) {
